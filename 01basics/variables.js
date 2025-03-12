aba847942af04dbd6e9d99f7437f126b31316d2e
Faizan Awan <134684325+faizan-awan@users.noreply.github.com>
2025-03-12 04:17:13 +0000

    variables of js

---
+++ 01basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 240430787
+let accountEmail = "faizanawan@gmail.com"
+var accountPassword = "54321"
+accountCity = "Lahore"
+let accountState
+
+accountEmail = "faizan@gmail.com"
+accountPassword = "11223344"
+accountCity = "Lahore Punjab"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState,])
