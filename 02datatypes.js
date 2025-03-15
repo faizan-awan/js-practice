56c1e3ca19690e4cd450e30332c0a6e856e74b9e
Faizan Awan <134684325+faizan-awan@users.noreply.github.com>
2025-03-15 14:34:30 +0000

    datatypes

---
+++ 02datatypes.js
@@ -0,0 +1,11 @@
+let yourName = "Faizan"
+let yourAge = 21
+let yourId = 36127
+let islogIn = true
+
+
+// console.table([ yourName, yourAge, yourId, islogIn])
+
+ console.log(yourName);
+ 
+// console.table([yourName, yourAge, yourId, islogIn]); 
